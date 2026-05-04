ddde2d7fa6d9b4a1774d0de83b75eeee7054ce78
Max Felipe <maxsfs11@gmail.com>
2026-05-04 13:50:46 +0000

    Feat: Adicionando Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,23 @@ import Courses from "./components/Courses";
 import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
 import BeforeAfter from "./components/BeforeAfter";
+import { Analytics } from '@vercel/analytics/react';
+
+function App() {
+  return (
+    <>
+      {/* Seus outros componentes (Header, Hero, Footer, etc) */}
+      <Router>
+        <Routes>
+           {/* Suas rotas aqui */}
+        </Routes>
+      </Router>
+
+      {/* Adicione o rastreador aqui no final */}
+      <Analytics />
+    </>
+  );
+}
 
 function App() {
   return (
